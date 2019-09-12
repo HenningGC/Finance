8771b14db4fa38b5eb08aa595d2403a2052b0a78
meteogold <55255975+meteogold@users.noreply.github.com>
2019-09-12 21:18:00 +0200

    Create Compunded Interest Python.py

---
+++ Compunded Interest Python.py
@@ -0,0 +1,16 @@
+p= input('Enter value of Principle')
+n= input('Enter value of Years')
+r= input('Enter value of Interest Rate')
+
+p= int(p)
+n= int(n)
+r= int(r)
+
+r= (r/100)+1
+
+ci= p*((r**n)-1)
+
+print ("You will get:",ci, 'in return')
+ci= int(ci)
+cit= p+ci
+print ("You will then have a total of:",cit, 'available')
