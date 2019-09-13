aea5b6a87b57cace73423e55753fa6aac835d4e4
meteogold <55255975+meteogold@users.noreply.github.com>
2019-09-13 21:44:59 +0200

    Update Compound Interest Python.py

--- 2019/Compound Interest Python.py
+++ 2019/Compound Interest Python.py
@@ -1,3 +1,5 @@
+#This is my first python code related to finance, it is a simple script that allows you to calculate the compounded interest of a Principle.
+#Created on the 12th of September 2019
 p= input('Enter value of Principle')
 n= input('Enter value of Years')
 r= input('Enter value of Interest Rate')
