20cc15679eb2e79a72f0f399f3304367f2d431da
Henning Gruhl <55255975+HenningGC@users.noreply.github.com>
2019-09-15 18:54:36 +0200

    Update Compound Interest Python.py

--- 2019/Compound Interest Python.py
+++ 2019/Compound Interest Python.py
@@ -1,4 +1,4 @@
-#This is my first python code related to finance, it is a simple script that allows you to calculate the compounded interest of a Principle.
+#This is my first python code related to finance, it is a simple script that allows you to calculate the compound interest of a Principle.
 #Created on the 12th of September 2019
 p= input('Enter value of Principle')
 n= input('Enter value of Years')
